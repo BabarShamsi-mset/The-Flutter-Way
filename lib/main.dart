843be7e849828a4f41fb4186d7950ee464eccb5c
Babar <babar@aymcommerce.com>
2023-11-11 15:17:48 +0500

    Initail Commit, Project Created

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const IntroPage());
+}
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
+
